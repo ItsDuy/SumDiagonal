f7793b4e3430c936f5463b48a94861629a2543e1
ItsDuy <duy.nguyen280805@hcmut.edu.vn>
2024-08-07 14:25:05 +0700

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿namespace SumDiagonal;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Enter the size of the square matrix: ");
+        int size = int.Parse(Console.ReadLine());
+
+        double[,] matrix = new double[size, size];
+        Console.WriteLine("Enter the elements of the matrix:");
+        for (int i = 0; i < size; i++)
+        {
+            for (int j = 0; j < size; j++)
+            {
+                Console.Write($"Enter element at position ({i}, {j}): ");
+                matrix[i, j] = double.Parse(Console.ReadLine());
+            }
+        }
+        Console.WriteLine("Entered matrix:");
+        for (int i = 0; i < size; i++)
+        {
+            for (int j = 0; j < size; j++)
+            {
+                Console.Write(matrix[i, j] + "\t");
+            }
+            Console.WriteLine();
+        }
+
+        double sumDiagonal = 0;
+        for (int i = 0; i < size; i++)
+        {
+            sumDiagonal += matrix[i, i];
+        }
+
+        Console.WriteLine($"The sum of diagonal elements is {sumDiagonal}");
+    }
+}
+
